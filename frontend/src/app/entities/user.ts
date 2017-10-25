82f661f1577455a62c1a01ae21b94f4d694c0836
Janne Mäki <janne.maki@evident.fi>
2017-10-25 22:38:21 +0300

    No need to store apina user here

--- frontend/src/app/entities/user.ts
+++ frontend/src/app/entities/user.ts
@@ -1,22 +1,16 @@
-import { UserModel } from '../apina';
+import { User as ApinaUser} from '../apina';
 
 export class User {
 
   name: string;
   email: string;
   superuser: boolean;
-  userModel: UserModel;
 
-  constructor(userModel: UserModel) {
+  constructor(userModel: ApinaUser) {
     this.name = userModel.firstName + ' ' + userModel.lastName;
     this.email = userModel.email;
     this.superuser = userModel.superuser;
-    this.userModel = this.userModel;
   }
-
-  getUserModel(){
-    return this.userModel;
-  };
 
   print() {
     return `- = ${this.name} ${this.email} Admin: ${this.superuser ? 'Yes' : 'No'} = -`;

c62eccf83011af343c27f76b0aedeff9892b155d
lammitomi <tomi.lammi@gatewaytechnolabs.fi>
2017-11-08 14:34:43 +0200

    Added model for user-request

---
+++ web-api/src/main/java/fi/vm/yti/groupmanagement/model/UserRequest.java
@@ -0,0 +1,9 @@
+package fi.vm.yti.groupmanagement.model;
+
+import java.util.UUID;
+
+public class UserRequest {
+    public Integer id;
+    public String user_email;
+    public UUID organization_id;
+}

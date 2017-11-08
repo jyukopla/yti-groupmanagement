710d1cc919a49587d421da9bf4c4400928157ab4
lammitomi <tomi.lammi@gatewaytechnolabs.fi>
2017-11-08 14:40:29 +0200

    Api changes for accessing access requests

--- web-api/src/main/java/fi/vm/yti/groupmanagement/controller/UserRequestController.java
+++ web-api/src/main/java/fi/vm/yti/groupmanagement/controller/UserRequestController.java
@@ -2,15 +2,18 @@ package fi.vm.yti.groupmanagement.controller;
 
 
 import fi.vm.yti.groupmanagement.dao.UserRequestDao;
+import fi.vm.yti.groupmanagement.model.UserRequest;
 import fi.vm.yti.groupmanagement.model.UserRequestModel;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.UUID;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.web.bind.annotation.RequestMethod.PUT;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
 @RequestMapping("/api/request")
@@ -24,7 +27,16 @@ class UserRequestController {
 
     @RequestMapping(value = "{request}", method = PUT, consumes = APPLICATION_JSON_VALUE)
     public void setUser(@RequestBody UserRequestModel request) {
-
         this.userRequestDao.setUserRequest(request);
+    }
+
+    @RequestMapping(value = "/get/{id}", method = GET)
+    public List<UserRequest> getUserRequests(@PathVariable("id") UUID organizationId) {
+        return this.userRequestDao.getUserRequests(organizationId);
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = PUT)
+    public void deleteUserRequest(@PathVariable Integer id) {
+        this.userRequestDao.deleteUserRequest(id);
     }
 }
--- web-api/src/main/java/fi/vm/yti/groupmanagement/dao/UserRequestDao.java
+++ web-api/src/main/java/fi/vm/yti/groupmanagement/dao/UserRequestDao.java
@@ -1,9 +1,13 @@
 package fi.vm.yti.groupmanagement.dao;
 
+import fi.vm.yti.groupmanagement.model.UserRequest;
 import fi.vm.yti.groupmanagement.model.UserRequestModel;
 import org.dalesbred.Database;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
 
 
 @Repository
@@ -18,4 +22,14 @@ public class UserRequestDao {
     public void setUserRequest(UserRequestModel userRequest) {
         db.update("INSERT INTO request (user_email, organization_id) VALUES (?,?)", userRequest.email, userRequest.uuid);
     }
+
+    public List<UserRequest> getUserRequests(UUID organizationId) {
+        return db.findAll(UserRequest.class, "SELECT id, user_email, organization_id FROM request WHERE organization_id=?", organizationId);
+    }
+
+    public void deleteUserRequest(Integer requestId) {
+        db.update("DELETE FROM request WHERE id=?", requestId);
+
+    }
 }
+

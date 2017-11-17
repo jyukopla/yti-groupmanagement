13623e27df9ba5aea037b474e755c74db4ec9128
Janne Mäki <janne.maki@evident.fi>
2017-11-17 16:44:41 +0200

    Fix style errors

--- frontend/src/app/components/user-requests.component.ts
+++ frontend/src/app/components/user-requests.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { UserRequestService } from "../services/user-request.service";
-import { UserRequestWithOrganization } from "../apina";
-import { UserOrganizationService } from "../services/userorganization.service";
+import { Component } from '@angular/core';
+import { UserRequestService } from '../services/user-request.service';
+import { UserRequestWithOrganization } from '../apina';
+import { UserOrganizationService } from '../services/userorganization.service';
 
 @Component({
   selector: 'app-user-requests',
   template: `
     <div class="container">
       <div class="row">
-        <!--div class="col-md-2"-->
         <br>
           <div id="accessrequests">
            <h2 translate>Access requests</h2>            
@@ -31,14 +30,16 @@ import { UserOrganizationService } from "../services/userorganization.service";
                 <td>{{requests.role | translate}}</td>
                 <div class="col-md-4">
                 <td><i class="fa fa-trash fa-lg" (click)="removeRequest(requests)"></i></td>
-                <td><button id="acceptrequest" type="button" class="btn btn-default btn-sm" (click)="acceptUserRequest(requests)" translate>Add</button></td>
+                <td><button id="acceptrequest" 
+                            type="button" 
+                            class="btn btn-default btn-sm" 
+                            (click)="acceptUserRequest(requests)" translate>Add</button>
+                </td>
                 </div>
               </tr>
               </tbody>
             </table>
-
           </div>
-        <!--/div-->
       </div>
     </div>
   `,
@@ -56,7 +57,6 @@ export class UserRequestsComponent {
     });
   }
 
-
   removeRequest(userRequest: UserRequestWithOrganization) {
     this.userRequestService.deleteRequest(userRequest.id).subscribe(() => {
       this.userRequests.splice(this.userRequests.indexOf(userRequest), 1);
@@ -64,16 +64,13 @@ export class UserRequestsComponent {
   }
 
   acceptUserRequest(userRequest: UserRequestWithOrganization) {
-    console.log("Adding user");
+    console.log('Adding user');
     console.log(userRequest.email);
     this.userOrganizationService.addUserToOrganization(userRequest).subscribe(() => {
       this.userRequestService.deleteRequest(userRequest.id).subscribe(() => {
         this.userRequests.splice(this.userRequests.indexOf(userRequest), 1);
       });
-      //Add notification for new user in organization
+      // Add notification for new user in organization
     });
-
   }
-
-
 }

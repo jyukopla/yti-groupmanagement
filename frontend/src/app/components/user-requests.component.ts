08cd04982fcef0e0ac1cad99cf77c20d191da90a
lammitomi <tomi.lammi@gatewaytechnolabs.fi>
2017-11-21 13:10:34 +0200

    button for demo purpose

--- frontend/src/app/components/user-requests.component.ts
+++ frontend/src/app/components/user-requests.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../services/api.service';
-import { UserRequestWithOrganization } from '../apina';
+import {UserRequestModel, UserRequestWithOrganization} from '../apina';
 
 @Component({
   selector: 'app-user-requests',
@@ -10,7 +10,7 @@ import { UserRequestWithOrganization } from '../apina';
         <br>
         <div id="accessrequests">
           <h2 translate>Access requests</h2>
-          <p *ngIf="userRequests.length === 0" translate>No requests</p>
+          <p *ngIf="userRequests.length === 0" translate>No requests</p>          
           <table *ngIf="userRequests.length > 0">
             <thead>
             <tr>
@@ -42,6 +42,11 @@ import { UserRequestWithOrganization } from '../apina';
           </table>
         </div>
       </div>
+      <button id="demobutton"
+              type="button"
+              class="btn btn-default btn-sm"
+              (click)="addrequest()">DEMO
+      </button>
     </div>
   `,
   styleUrls: ['./user-requests.component.scss']
@@ -49,6 +54,10 @@ import { UserRequestWithOrganization } from '../apina';
 export class UserRequestsComponent {
 
   userRequests: UserRequestWithOrganization[] = [];
+
+  req1 = new UserRequestModel;
+  req2 = new  UserRequestModel;
+  req3 = new UserRequestModel;
 
   constructor(private apiService: ApiService) {
 
@@ -70,5 +79,27 @@ export class UserRequestsComponent {
       this.userRequests.splice(this.userRequests.indexOf(userRequest), 1);
     });
     // Add notification for new user in organization
+  }
+
+  // FOR DEMO PURPOSE
+  addrequest() {
+
+
+
+    this.req1.role = "TERMINOLOGY_EDITOR"
+    this.req1.email = "jere.veijalainen@riodigital.fi";
+    this.req1.organizationId = "09bf60ab-cca0-48b6-ab45-0a7f27ff9f88";
+
+    this.req2.role = "ADMIN"
+    this.req2.email = "tomi.lammi@gatewaytechnolabs.fi";
+    this.req2.organizationId = "09bf60ab-cca0-48b6-ab45-0a7f27ff9f88";
+
+    this.req3.role = "ADMIN"
+    this.req3.email = "tomi.lammi@gatewaytechnolabs.fi";
+    this.req3.organizationId = "2f9eab0b-d6f0-42ce-b3aa-9f3647618b4d";
+
+    this.apiService.createRequest(this.req1).subscribe();
+    this.apiService.createRequest(this.req2).subscribe();
+    this.apiService.createRequest(this.req3).subscribe();
   }
 }

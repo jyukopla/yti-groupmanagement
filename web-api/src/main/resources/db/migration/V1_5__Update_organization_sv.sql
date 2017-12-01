497042847defe88e4c62d2935c78411dab7c8b50
lammitomi <tomi.lammi@gatewaytechnolabs.fi>
2017-12-01 09:19:40 +0200

    Updated swedish names to organizations in DB

---
+++ web-api/src/main/resources/db/migration/V1_5__Update_organization_sv.sql
@@ -0,0 +1,5 @@
+UPDATE organization SET name_sv='Institutet för hälsa och välfärd' WHERE id='2f9eab0b-d6f0-42ce-b3aa-9f3647618b4d';
+UPDATE organization SET name_sv='Befolkningsregistercentralen' WHERE id='d9c76d52-03d3-4480-8c2c-b66e6d9c57f2';
+UPDATE organization SET name_sv='CSC - IT-centret för vetenskap' WHERE id='d8c0f756-3a05-4f77-8180-142f64772a17';
+UPDATE organization SET name_sv='Utvecklare av interoperabilitetsplattformar' WHERE id='7d3a3c00-5a6b-489b-a3ed-63bb58c26a63';
+UPDATE organization SET name_sv='Konceptuell modelleringsgrupp för offentlig förvaltning' WHERE id='09bf60ab-cca0-48b6-ab45-0a7f27ff9f88';

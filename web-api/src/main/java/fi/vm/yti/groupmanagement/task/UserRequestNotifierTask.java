c9bf79d67c408bb19849701efdb589d13fcb2c81
Janne Mäki <janne.maki@evident.fi>
2017-12-11 17:36:42 +0200

    Make email sending configurable

--- web-api/src/main/java/fi/vm/yti/groupmanagement/task/UserRequestNotifierTask.java
+++ web-api/src/main/java/fi/vm/yti/groupmanagement/task/UserRequestNotifierTask.java
@@ -3,6 +3,7 @@ package fi.vm.yti.groupmanagement.task;
 import fi.vm.yti.groupmanagement.service.EmailSenderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +13,19 @@ public class UserRequestNotifierTask {
     private static Logger log = LoggerFactory.getLogger(UserRequestNotifierTask.class);
 
     private final EmailSenderService emailSenderService;
+    private final boolean sendAdminEmails;
 
-    public UserRequestNotifierTask(EmailSenderService emailSenderService) {
+    public UserRequestNotifierTask(EmailSenderService emailSenderService,
+                                   @Value("${send.admin.emails}") boolean sendAdminEmails) {
         this.emailSenderService = emailSenderService;
+        this.sendAdminEmails = sendAdminEmails;
     }
 
     @Scheduled(cron = "0 0/5 * * * ?")
     public void notifyAdmins() {
-        log.debug("Scheduled job started");
-        this.emailSenderService.sendEmailsToAdmins();
+        if (sendAdminEmails) {
+            log.debug("Scheduled admin notification task started");
+            this.emailSenderService.sendEmailsToAdmins();
+        }
     }
 }

259a1cab062d9a645dd3a5a16682aa1c91a583bf
lammitomi <tomi.lammi@gatewaytechnolabs.fi>
2017-12-11 11:18:39 +0200

    One more try to get the icons to show up in IE, set no-cache

--- frontend/src/app/components/edit-organization.component.ts
+++ frontend/src/app/components/edit-organization.component.ts
@@ -11,6 +11,9 @@ import { ApiService } from '../services/api.service';
   selector: 'app-edit-organization',
   styleUrls: ['./edit-organization.component.scss'],
   template: `
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <div class="content-box" *ngIf="organization">
 
       <app-back-button (back)="back()"></app-back-button>

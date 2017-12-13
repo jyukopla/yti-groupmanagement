2d6d7254027ae37aaa2c6a9499845a06d48fd00d
lammitomi <tomi.lammi@gatewaytechnolabs.fi>
2017-12-13 10:32:38 +0200

    Modal dialog for user removal from organization

---
+++ frontend/src/app/components/delete-confirmation-modal.component.ts
@@ -0,0 +1,62 @@
+import { Component, Injectable, Input } from '@angular/core';
+import {NgbActiveModal, NgbModal} from "@ng-bootstrap/ng-bootstrap";
+
+
+@Injectable()
+export class DeleteConfirmationModalService {
+
+  constructor(private modalService: NgbModal) {
+  }
+
+  open(username: string): Promise<any> {
+    const modalRef = this.modalService.open(DeleteConfirmationModalComponent, { size: 'sm' });
+    const instance = modalRef.componentInstance as DeleteConfirmationModalComponent;
+    instance.username = username;
+    return modalRef.result;
+  }
+}
+
+
+@Component({
+  selector: 'app-delete-confirmation-modal',
+  styleUrls: ['./delete-confirmation-modal.component.scss'],
+  template: `
+    <div class="modal-header modal-header-warning">
+      <h4 class="modal-title">
+        <a><i class="fa fa-times" (click)="cancel()"></i></a>
+        <span translate>Confirm remove</span>
+      </h4>
+    </div>
+    <div class="modal-body">
+      <div class="row">
+        <div class="col-md-12">
+          
+            <p translate>This user will be removed from organization after the changes are saved.</p>          
+            <li>{{username}}</li>
+          <br>
+          <span translate>Are you sure that you want to remove?</span>
+        </div>
+      </div>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary confirm" (click)="confirm()" translate>Yes</button>
+      <button type="button" class="btn btn-default cancel" (click)="cancel()" translate>Cancel</button>
+    </div>
+  `
+})
+
+export class DeleteConfirmationModalComponent {
+
+  @Input() username: string;
+
+  constructor(private modal: NgbActiveModal) {
+  }
+
+  cancel() {
+    this.modal.dismiss('cancel');
+  }
+
+  confirm() {
+    this.modal.close();
+  }
+}

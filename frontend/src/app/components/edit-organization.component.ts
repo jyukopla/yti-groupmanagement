2779b7013ebe05e89e5a3f88c85b2cb4f2bb5270
lammitomi <tomi.lammi@gatewaytechnolabs.fi>
2017-12-15 13:20:50 +0200

    Minor cleanup

--- frontend/src/app/components/edit-organization.component.ts
+++ frontend/src/app/components/edit-organization.component.ts
@@ -138,9 +138,7 @@ export class EditOrganizationComponent {
 
   removeUser(user: UserViewModel) {
     this.deleteUserModal.open(user.name)
-
       .then(() => (this.users.splice(this.users.indexOf(user)), 1, this.hasDetailsChanged = true)).catch(ignoreModalClose);
-    //this.hasDetailsChanged = true;
   }
 
   saveOrganization() {

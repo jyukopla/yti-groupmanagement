48ff730be4780d9f3b135f9d707986e24711ba9b
lammitomi <tomi.lammi@gatewaytechnolabs.fi>
2018-02-09 10:09:05 +0200

    Logging added for Frontend and PublicApi controllers

--- web-api/src/main/java/fi/vm/yti/groupmanagement/controller/FrontendController.java
+++ web-api/src/main/java/fi/vm/yti/groupmanagement/controller/FrontendController.java
@@ -10,11 +10,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 @RequestMapping("/api")
@@ -22,6 +27,7 @@ public class FrontendController {
 
     private final FrontendService frontendService;
     private final AuthenticatedUserProvider userProvider;
+    private static final Logger logger = LoggerFactory.getLogger(FrontendController.class);
 
     @Autowired
     public FrontendController(FrontendService frontendService,
@@ -32,61 +38,77 @@ public class FrontendController {
 
     @RequestMapping(value = "/authenticated-user", method = GET, produces = APPLICATION_JSON_VALUE)
     public YtiUser getAuthenticatedUser() {
+        logger.info("getAuthenticatedUser requested");
         return userProvider.getUser();
     }
     
     @RequestMapping(value = "/organizations", method = GET, produces = APPLICATION_JSON_VALUE)
     public List<OrganizationListItem> getOrganizations() {
+        logger.info("getOrganizations requested");
         return this.frontendService.getOrganizationList();
     }
 
     @RequestMapping(value = "/organization/{id}", method = GET, produces = APPLICATION_JSON_VALUE)
     public OrganizationWithUsers getOrganization(@PathVariable("id") UUID id) {
+        logger.info("getOrganization requested with id: " + id.toString());
         return this.frontendService.getOrganization(id);
     }
 
     @RequestMapping(value = "/organization", method = POST, produces = APPLICATION_JSON_VALUE)
     public UUID createOrganization(@RequestBody CreateOrganization createOrganization) {
+        logger.info("createOrganization requested with descriptionFI: " + createOrganization.descriptionFi + ", " +
+                "nameFI: " + createOrganization.nameFi + " and adminUserEmails: {}", createOrganization.adminUserEmails.toString());
         return this.frontendService.createOrganization(createOrganization);
     }
 
     @RequestMapping(value = "/organization", method = PUT, produces = APPLICATION_JSON_VALUE)
     public void updateOrganization(@RequestBody UpdateOrganization updateOrganization) {
+        List<EmailRole> userRoles = updateOrganization.userRoles;
+        List<String> emailRoles = new ArrayList<>();
+        for(EmailRole item : userRoles) { emailRoles.add(new String(item.userEmail +": " + item.role));}
+        logger.info("updateOrganization requested with id: " + updateOrganization.organization.id + " , nameFI: " + updateOrganization.organization.nameFi + ", userRoles: {}", emailRoles);
         this.frontendService.updateOrganization(updateOrganization);
     }
 
     @RequestMapping(value = "/roles", method = GET, produces = APPLICATION_JSON_VALUE)
     public List<String> getAllRoles() {
+        logger.info("getAllRoles requested");
         return this.frontendService.getAllRoles();
     }
 
     @RequestMapping(value = "/usersForOwnOrganizations", method = GET, produces = APPLICATION_JSON_VALUE)
     public List<UserWithRolesInOrganizations> getUsersForOwnOrganizations() {
+        logger.info("getUsersForOwnOrganizations requested");
         return this.frontendService.getUsersForOwnOrganizations();
     }
 
     @RequestMapping(value = "/users", method = GET, produces = APPLICATION_JSON_VALUE)
     public List<UserWithRolesInOrganizations> getUsers() {
+        logger.info("getUsers requested");
         return this.frontendService.getUsers();
     }
 
     @RequestMapping(value = "/requests", method = GET, produces = APPLICATION_JSON_VALUE)
     public List<UserRequestWithOrganization> getAllUserRequests() {
+        logger.info("getAllUserRequests requested");
         return this.frontendService.getAllUserRequests();
     }
 
     @RequestMapping(value = "/request", method = POST, consumes = APPLICATION_JSON_VALUE)
     public void addUserRequest(@RequestBody UserRequestModel request) {
+        logger.info("addUserRequest requested for email: " + request.email + ", role: " + request.role + " for organization id: " + request.organizationId);
         this.frontendService.addUserRequest(request);
     }
 
     @RequestMapping(value = "/request/{id}", method = DELETE)
     public void declineUserRequest(@PathVariable("id") int id) {
+        logger.info("declineUserRequest requested with request id: " + id);
         this.frontendService.declineUserRequest(id);
     }
 
     @RequestMapping(value = "/request/{id}", method = POST)
     public void acceptUserRequest(@PathVariable("id") int id) {
+        logger.info("acceptUserRequest requested with request id: " + id);
         this.frontendService.acceptUserRequest(id);
     }
 }
--- web-api/src/main/java/fi/vm/yti/groupmanagement/controller/PublicApiController.java
+++ web-api/src/main/java/fi/vm/yti/groupmanagement/controller/PublicApiController.java
@@ -7,6 +7,8 @@ import fi.vm.yti.groupmanagement.model.PublicApiUserRequest;
 import fi.vm.yti.groupmanagement.service.PublicApiService;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +26,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 public class PublicApiController {
 
     private final PublicApiService publicApiService;
+    private static final Logger logger = LoggerFactory.getLogger(PublicApiController.class);
 
     public PublicApiController(PublicApiService publicApiService) {
         this.publicApiService = publicApiService;
@@ -33,7 +36,7 @@ public class PublicApiController {
     public PublicApiUser getUser(@RequestParam @NotNull String email,
                                  @RequestParam(required = false) @Nullable String firstName,
                                  @RequestParam(required = false) @Nullable String lastName) {
-
+        logger.info("getUser requested with email: " + email +", and name: " + firstName + " " + lastName);
         if (email.isEmpty()) {
             throw new RuntimeException("Email is a mandatory parameter");
         }
@@ -47,12 +50,14 @@ public class PublicApiController {
 
     @RequestMapping(value = "/users", method = GET, produces = APPLICATION_JSON_VALUE)
     public List<PublicApiUserListItem> getUsers() {
+        logger.info("getUsers requested");
         return this.publicApiService.getUsers();
     }
 
     @RequestMapping(value = "/organizations", method = GET, produces = APPLICATION_JSON_VALUE)
     @CrossOrigin
     public List<PublicApiOrganization> getOrganizations() {
+        logger.info("getOrganizations requested");
         return publicApiService.getOrganizations();
     }
 
@@ -60,12 +65,13 @@ public class PublicApiController {
     public void addUserRequest(@RequestParam String email,
                                @RequestParam UUID organizationId,
                                @RequestParam String role) {
-
+        logger.info("addUserRequest requested with email: " + email + " for organization id: " + organizationId + " for role: " + role);
         this.publicApiService.addUserRequest(email, organizationId, role);
     }
 
     @RequestMapping(value = "/requests", method = GET, produces = APPLICATION_JSON_VALUE)
     public List<PublicApiUserRequest> getUserRequests(@RequestParam String email) {
+        logger.info("getUserRequests requested with email: " + email);
         return this.publicApiService.getUserRequests(email);
     }
 }

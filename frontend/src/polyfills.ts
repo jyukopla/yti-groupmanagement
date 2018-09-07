3802f96de4771dd26f8c0e94ef8705b6d86e0378
lammitomi <tomi.lammi@gatewaytechnolabs.fi>
2018-09-07 09:49:46 +0300

    Fix for undefined process

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -46,6 +46,9 @@ import 'core-js/es6/reflect';
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es7/reflect';
 
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
 
 /**
  * Web Animations `@angular/platform-browser/animations`
